0f6382b9e5ace473df26aa8f691e79ca0be24ff3
mallerp <zhaosenlin@qq.com>
2014-09-11 22:50:24 +0800

    Update backuperp.sh

--- scripts/backuperp.sh
+++ scripts/backuperp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 SqlBakName=newerpsql_$(date +%y%m%d).sql
 cd /etc/rc.d/init.d/
-mysqldump -uroot -plove521.. newerp> /backup/$SqlBakName
+mysqldump -umallerp -ppassword newerp> /backup/$SqlBakName
 cd /backup
 ftp -nv 192.168.0.35 << EOF
 user mallerp mallerpftp
